cca4abfc34638dc00cb02520dc27263d90405efc
USER <seagate90@mail.ru>
2022-10-09 14:53:00 +0400

    added2

--- src/test/java/demoqa/AutoPracticeTest.java
+++ src/test/java/demoqa/AutoPracticeTest.java
@@ -1,12 +1,13 @@
 package demoqa;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 
 public class AutoPracticeTest extends TestBase {
