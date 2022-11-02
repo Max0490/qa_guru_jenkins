7651ee5608b1d0d0dfcf76a2ebc2eafc23f89167
USER <seagate90@mail.ru>
2022-11-02 18:31:44 +0400

    add lambda

--- src/test/java/demoqa/AutoPracticeEtalon.java
+++ src/test/java/demoqa/AutoPracticeEtalon.java
@@ -8,6 +8,7 @@ import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class AutoPracticeEtalon extends TestBase {
 
@@ -19,37 +20,43 @@ public class AutoPracticeEtalon extends TestBase {
 
     @Test
     void FillFormTests1() {
-        open("/automation-practice-form");
-        $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
 
-        executeJavaScript("$('footer').remove()");
-        executeJavaScript("$('#fixedban').remove()");
-        executeJavaScript("$('#RightSide_Advertisement').remove()");
+        step("Opening test form", () -> {
+            open("/automation-practice-form");
+            $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            executeJavaScript("$('footer').remove()");
+            executeJavaScript("$('#fixedban').remove()");
+            executeJavaScript("$('#RightSide_Advertisement').remove()");
+        });
 
-        $("#firstName").setValue("Maxim");
-        $("#lastName").setValue("Maximov");
-        $("#userEmail").setValue("maxim@maxim.com");
-        $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("1234567890");
-        $("#dateOfBirthInput").click();
-        $(".react-datepicker__month-select").selectOption("April");
-        $(".react-datepicker__year-select").selectOption("1990");
-        $(".react-datepicker__day--026:not(.react-datepicker__day--outside-month)").click();
-        $("#subjectsInput").setValue("Math").pressEnter();
-        $("#hobbiesWrapper").$(byText("Sports")).click();
-        //$("#uploadPicture").uploadFromClasspath("img/1.png");
-        $("#currentAddress").setValue("Some address 1");
-        $("#state").click();
-        $("#stateCity-wrapper").$(byText("NCR")).click();
-        $("#city").click();
-        $("#stateCity-wrapper").$(byText("Delhi")).click();
-        $("#submit").click();
+        step("Fill form", () -> {
+            $("#firstName").setValue("Maxim");
+            $("#lastName").setValue("Maximov");
+            $("#userEmail").setValue("maxim@maxim.com");
+            $("#genterWrapper").$(byText("Male")).click();
+            $("#userNumber").setValue("1234567890");
+            $("#dateOfBirthInput").click();
+            $(".react-datepicker__month-select").selectOption("April");
+            $(".react-datepicker__year-select").selectOption("1990");
+            $(".react-datepicker__day--026:not(.react-datepicker__day--outside-month)").click();
+            $("#subjectsInput").setValue("Math").pressEnter();
+            $("#hobbiesWrapper").$(byText("Sports")).click();
+            //$("#uploadPicture").uploadFromClasspath("img/1.png");
+            $("#currentAddress").setValue("Some address 1");
+            $("#state").click();
+            $("#stateCity-wrapper").$(byText("NCR")).click();
+            $("#city").click();
+            $("#stateCity-wrapper").$(byText("Delhi")).click();
+            $("#submit").click();
+        });
 
-        $(".modal-dialog").should(appear);
-        $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+        step("Cheking results", () -> {
+            $(".modal-dialog").should(appear);
+            $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+            $(".table-responsive table").shouldHave(text("Maxim"), text("Maximov"),
+                    text("maxim@maxim.com"), text("1234567890"));
+        });
 
-        $(".table-responsive table").shouldHave(text("Maxim"), text("Maximov"),
-                text("maxim@maxim.com"), text("1234567890"));
 
     }
 }
